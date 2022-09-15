841618fcb5536e0178668e017ae718f2a57ddac1
Ryan Gosine <ryanganeshgosineyarn test exercise-6git config --global user.email ryanganeshgosine>
2022-09-15 19:55:03 -0400

    Login and Register Pages 90% (just need futher styling and routing)

--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { EntryPage, PageHeader, PageHolder } from "./style";
+import EntryCard from "../EntryCard/EntryCard";
+import InputGroup from "../InputGroup/InputGroup";
+import Input from "../Input";
+import Button from "../Button";
 
 const Login = () => {
   return (
-    <LoginWrapper>
-      <h1>Login!</h1>
-      <LinkOne>
-        Don't have an account?
-        <Link to="register">Register</Link>
-      </LinkOne>
-      <LinkTwo>
-        <Link to="/">Go Back</Link>
-      </LinkTwo>
-    </LoginWrapper>
+    <EntryPage>
+      <PageHeader to="/">SOME TEXT HERE</PageHeader>
+      <EntryCard>
+        <h2>Log In</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <InputGroup>
+            <label htmlFor="login-email">Email Address</label>
+            <Input type="text" placeholder="name@email.com" id="login-email" />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="password">Password</label>
+            <Input type="password" placeholder="Password" id="password" />
+          </InputGroup>
+          <Button type="submit" full>
+            Log In
+          </Button>
+        </form>
+        <span>
+          Haven't ignighted your spark?
+          <Link to="signup">Sign Up</Link>
+        </span>
+      </EntryCard>
+    </EntryPage>
   );
 };
 
-const LoginWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const LinkOne = styled.ul``;
-const LinkTwo = styled.ul``;
+// const LoginWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
+// const LinkOne = styled.ul``;
+// const LinkTwo = styled.ul``;
 export default Login;
--- client/src/components/Pages/Register.js
+++ client/src/components/Pages/Register.js
@@ -1,23 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { EntryPage, PageHeader } from "./style";
+import EntryCard from "../EntryCard/EntryCard";
+import InputGroup from "../InputGroup/InputGroup";
+import Input from "../Input";
+import Button from "../Button";
 
 const Register = () => {
   return (
-    <RegisterWrapper>
-      <h1>Enter the Multiverse</h1>
-      <span>
-        Already have an Account?
-        <Link to="login">Log In</Link>
-      </span>
-      <br />
-      <span>
-        <Link to="/">Go Back</Link>
-      </span>
-    </RegisterWrapper>
+    <EntryPage>
+      <PageHeader to="/">Back To Home</PageHeader>
+      <EntryCard>
+        <h2>Enter The Multiverse</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <InputGroup>
+            <label htmlFor="signup-name">Full Name</label>
+            <Input type="text" placeholder="Jace Beleran" id="signup-name" />
+          </InputGroup>
+
+          <InputGroup>
+            <label htmlFor="signup-email">Email Address</label>
+            <Input
+              type="text"
+              placeholder="Jace@iwouldliketorespond.com"
+              id="signup-email"
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <label htmlFor="signup-password">Password</label>
+            <Input type="text" placeholder="bR@iNsT0rM!" id="signup-password" />
+          </InputGroup>
+          <Button type="submit" full>
+            Sign Up
+          </Button>
+        </form>
+        <span>
+          Already Planeswalking?
+          <Link to="login">Log In</Link>
+        </span>
+      </EntryCard>
+    </EntryPage>
   );
 };
-
-const RegisterWrapper = styled.div``;
 
 export default Register;
