4d47beffc14ce7658a0301b612e08c5411c77d06
Ryan Gosine <ryanganeshgosineyarn test exercise-6git config --global user.email ryanganeshgosine>
2022-09-16 12:13:21 -0400

    cardFoundPage updated

--- client/src/components/CardContext.js
+++ client/src/components/CardContext.js
@@ -1,5 +1,5 @@
 import React, { createContext } from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export const CardContext = createContext();
 
--- client/src/components/Pages/CardFound.js
+++ client/src/components/Pages/CardFound.js
@@ -24,15 +24,79 @@ const CardFound = () => {
   return (
     <PageWrapper>
       <Header />
+      <DetailsWrapper>
+        <NameAndCardImage>
+          <Name>{cardDisplay.data.name}</Name>
+          <CardImage src={cardDisplay.data.imageUrl} />
+        </NameAndCardImage>
 
-      <CardImage src={cardDisplay.data.imageUrl} />
-
+        <CardDetails>
+          <Title>Details</Title>
+          <ConvertedManaCost>
+            Mana Cost: {cardDisplay.data.manaCost}
+          </ConvertedManaCost>
+          <ManaCost>Converted Mana Cost: {cardDisplay.data.cmc}</ManaCost>
+          <OracleText>Card Text: {cardDisplay.data.text}</OracleText>
+          <Type>Types: {cardDisplay.data.types}</Type>
+          <Rarity>Rarity: {cardDisplay.data.rarity}</Rarity>
+          <Artist>Artist: {cardDisplay.data.artist}</Artist>
+        </CardDetails>
+      </DetailsWrapper>
       <Footer />
     </PageWrapper>
   );
 };
 
 const PageWrapper = styled.div``;
+
+const DetailsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const NameAndCardImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+`;
+
+const CardDetails = styled.li`
+  display: flex;
+  flex-direction: column;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
+  padding: 40px;
+`;
+
+const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  font-size: 1.5rem;
+  margin-bottom: 20px;
+`;
 const CardImage = styled.img``;
+const ConvertedManaCost = styled.p`
+  padding: 10px;
+`;
+const Name = styled.h1`
+  padding: 20px;
+  font-size: 2rem;
+`;
+const OracleText = styled.p`
+  padding: 10px;
+`;
+const ManaCost = styled.p`
+  padding: 10px;
+`;
+const Type = styled.p`
+  padding: 10px;
+`;
+const Rarity = styled.p`
+  padding: 10px;
+`;
+
+const Artist = styled.p`
+  padding: 10px;
+`;
 
 export default CardFound;
